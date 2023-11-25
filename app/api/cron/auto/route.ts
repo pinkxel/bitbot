068226f8d1e5f6c9a0a9edd64caf98a43b43c244
Rodrigo Yrigoyen Gonzales <rodry_yg@hotmail.com>
2023-11-25 14:11:09 -0500

    test cron job x3

--- app/api/cron/auto/route.ts
+++ app/api/cron/auto/route.ts
@@ -1,7 +1,6 @@
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
-  console.log('BITBOT v2')
+export async function GET(req, res) {
   if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json({ ok: false });
   }
